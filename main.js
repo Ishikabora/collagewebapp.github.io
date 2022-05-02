70ea7de38b21cc88278d6d569e25c8f474cec2f5
Ishikabora <96299273+Ishikabora@users.noreply.github.com>
2022-05-02 13:00:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+ 
+function start(){
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if(Content == "take my selfie"){
+        console.log("taking selfie ---")
+        speak();
+    }
+    
+    
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+    speak_data = "Taking your selfie in 5 seconds.";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+      img_id = "result1"
+      take_snapshot();
+      speak_data = "Taking your selfie in 5 seconds.";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    },5000);
+
+}
+setTimeout(function()
+    {
+      img_id = "result2"
+      take_snapshot();
+      speak_data = "Taking your selfie in 10 seconds.";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    },10000);
+
+    
+setTimeout(function()
+    {
+      img_id = "result3"
+      take_snapshot();
+      speak_data = "Taking your selfie in 15 seconds.";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    },15000);
+
+
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function take_snapshot(){
+    console.log(img_id);
+
+    Webcam.snap(function(data_uri){
+        if(img_id == "result1"){
+            document.getElementById("result1").innerHTML = '<img id = "result1" src = "'+data_uri+'"/>';
+         }
+         if(img_id == "result2"){
+            document.getElementById("result2").innerHTML = '<img id = "result2" src = "'+data_uri+'"/>';
+         }
+         if(img_id == "result3"){
+            document.getElementById("result3").innerHTML = '<img id = "result3" src = "'+data_uri+'"/>';
+         }
+    });
+}
